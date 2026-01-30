37999854773cda88d538d51258a199d39925850e
monikabaabu <monikababu207@gmail.com>
2026-01-30 23:08:40 +0530

    login

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,36 @@
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div className="login-page">
+      <div className="login-card">
+        <h2>Welcome Back 👋</h2>
+        <p>Please login to continue</p>
+
+        <form>
+          <div className="input-group">
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+
+          <div className="input-group">
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
